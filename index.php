fb46331d9c600ecf405d658d93f05c0d595fa503
mhsUPGRIS <mhs20670131@upgris.ac.id>
2023-04-13 23:01:51 +0700

    Biodata Diri

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/style.css" type="text/css">
+  <title>Biodata Diri</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Biodata Diri</h1>
+		<div class="img-container">
+			<img src="assets/3x4.jpg" alt="3x4">
+		</div>
+		<div class="row">
+			<div class="col-4">
+				<label>Nama:</label>
+				<p>Moh Adib Nur Rachmad</p>
+			</div>
+			<div class="col-4">
+				<label>TTL:</label>
+				<p>Kendal, 5 Juli 2001</p>
+			</div>
+			<div class="col-4">
+				<label>Jenis Kelamin:</label>
+				<p>Laki-laki</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-4">
+				<label>Alamat:</label>
+				<p>Ds. Tlahab RT 01 RW 03 Kec. Gemuh Kab. Kendal Jawa Tengah 51356</p>
+			</div>
+			<div class="col-4">
+				<label>Riwayat Pendidikan:</label>
+				<ul>
+					<li>SD Negeri 1 Weleri</li>
+					<li>SMP Negeri 1 Weleri</li>
+					<li>SMK Negeri 2 Kendal</li>
+					<li>Universitas PGRI Semarang</li>
+				</ul>
+			</div>
+			<div class="col-4">
+				<label>Hobby:</label>
+				<ul>
+					<li>Bermain Game</li>
+					<li>Bermain Game</li>
+					<li>Bermain Game</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
